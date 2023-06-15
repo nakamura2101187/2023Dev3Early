119e10da529b3f71dfffb2abfaa50b8acef4927a
uritake <2101145@s.asojuku.ac.jp>
2023-06-15 09:51:20 +0900

    バグ消し６/15

--- TimeThree/Create_acount/acnt_2.php
+++ TimeThree/Create_acount/acnt_2.php
@@ -1,5 +1,3 @@
-<?php
-?>
 <!DOCTYPE html>
 <html>
 <html lang="en">
@@ -7,6 +5,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+    .tema{
+        background-image: url(img/haikei2.jpg);
+    }
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
  <meta name="viewport" content="width=device-width,initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
@@ -19,24 +21,56 @@
 <link rel="stylesheet" type="text/css" href="../css/acnt_1.css">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
-    <style>
-    .tema{
-        background-image: url(img/haikei2.jpg);
-    }
+        <style>
     body{
        text-align: center;
        background-color: 	#F0E68C;
     } 
-    button {
-padding: 5px 10px; 
-display: block;
-margin: auto;
-border-radius: 15px;
-background-color:		#C0C0C0;
+    h1 {
+    font-family: "MS 明朝";  
+    text-align:center;
+    font-size : 15px; 
+    line-height : 4; 
+}
+    h2 {
+    font-family: "MS 明朝";     
+    text-align:center;
+    font-size : 15px; 
+    line-height : 4; 
+}
+h3 {
+    font-family: "MS 明朝";  
+    text-align:center;
+    font-size : 15px; 
+    line-height : 4; 
+}
+h4 {
+    font-family: "MS 明朝";  
+    text-align:center;
+    font-size : 15px; 
+    line-height : 4; 
+}
+button {
+    font-family: "MS 明朝";  
+    padding: 5px 10px; 
+    display: block;
+    margin: auto;
+    border-radius: 15px;
+    background-color:		#C0C0C0;
+}
+input{
+    font-family: "MS 明朝";   
+     padding: 5px 10px;
+     width: 600x;
+     border: 2px solid black;
+     background-color:	#C0C0C0;
+
 }
 </style>
 <body class="tema">
     <title>新規登録完了画面</title>
+
+        
 </head>
 <?php
 session_start();
@@ -44,24 +78,30 @@ session_start();
 require '../DAO.php';
 $dao = new DAO();
 
-$user = $dao ->insertUser($_SESSION['user']['mail'],$_SESSION["user"]["pass"]);
-if(isset($_SESSION["user"]["g_name"]) && isset($_SESSION["user"]["aikotoba"])){
+    $_SESSION['user'] =
+    ['mail' => $_POST['mail'],
+     'pass' => $_POST['pass'],
+     'name' => $_POST['name'],
+];
+    ?>
+        
+    
+     <h3>以下の内容で登録を完了しますか？</h3>
+     <h4>メールアドレス</h4>
+     <?php
+     echo '<h4>'. $_POST['mail'] .'</h4>';
+     ?>
+     <h4>パスワード</h4>
+     <?php
+     echo '<h4>'. $_POST['pass'] .'</h4>';
+     ?>
+     <h4>ニックネーム</h4>
+     <?php
+     echo '<h4>'. $_POST['name'] .'</h4>';
+     ?>
 
-}else{
-
-    echo "アカウントの登録が完了しました！";
-if(isset($_SESSION["user"]["g_name"]) && isset($_SESSION["user"]["aikotoba"])){
-
-}
-
-session_destroy();
-?>
 <form action="?" method="post"> 
-    <button type = "submit" formaction="../Login/test.php">登録する</button>
-</form>
-</body>
-</body>
-    <button type = "submit" formaction="../Login/login.php">登録する</button>
-</form>
+    <button type='submit' formaction="acnt_1.php">修正する</button>
+    <button type='submit' formaction="acnt_3.php">登録する</button>
 </form>
 </body>
--- timethree/Create_acount/acnt_3.php
+++ timethree/Create_acount/acnt_3.php
@@ -1,11 +1,6 @@
-<?php
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
  <meta name="viewport" content="width=device-width,initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
@@ -19,66 +14,38 @@
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
     <style>
-    .tema{
-        background-image: url(img/haikei2.jpg);
-    }
     body{
        text-align: center;
        background-color: 	#F0E68C;
     } 
     button {
-padding: 5px 10px; 
-display: block;
-margin: auto;
-border-radius: 15px;
-background-color:		#C0C0C0;
+    font-family: "MS 明朝";       
+    padding: 5px 10px; 
+    display: block;
+    margin: auto;
+    border-radius: 15px;
+    background-color:		#C0C0C0;
 }
 </style>
 <body class="tema">
     <title>新規登録完了画面</title>
+</head>
 <?php
-    $dao = new DAO();
+session_start();
 
-$user = $dao ->insertUser($_SESSION['user']['mail'],$_SESSION["user"]["pass"]);
-$group = $dao ->ginsertUser($_SESSION["user"]["aikotoba"],$_SESSION["user"]["name"]);
-$g = 0;
-
-
+require '../DAO.php';
+$dao = new DAO();
 
 $user = $dao ->insertUser($_SESSION['user']['mail'],$_SESSION["user"]["pass"],$_SESSION["user"]["name"]);
 
-if(isset($_SESSION["user"]["name"]) && isset($_SESSION["user"]["aikotoba"])){
-if(isset($_SESSION["user"]["g_name"]) && isset($_SESSION["user"]["aikotoba"])){
-    
-    $g_id = $dao ->g_id($_SESSION["user"]["aikotoba"],$_SESSION["user"]["name"]);
-    while($g == 0){
+    echo "アカウントの登録が完了しました！";
 
-    echo 'グループの登録が完了しました！！<br>';
-        $rand = rand(1000,9999);
-        $check_group = $dao -> check_group($rand);
-    
-        if(!isset($check_group["group_id"])){    
-            $group = $dao ->ginsertUser($rand,$_SESSION["user"]["aikotoba"],$_SESSION["user"]["g_name"]);
-            $g = $g+1;
-        }
-    
+    foreach($_SESSION['user'] as $row){
+
+        unset($row);
     }
-
-    echo 'グループの登録が完了しました！！<br>';
-    echo $rand;
-
-}else{
-if(isset($_SESSION["user"]["g_name"]) && isset($_SESSION["user"]["aikotoba"])){
-
-}
-
-session_destroy();
 ?>
 <form action="?" method="post"> 
-    <button type = "submit" formaction="../Login/test.php">登録する</button>
-</form>
-</body>
-</body>
-    <button type = "submit" formaction="../Login/login.php">登録する</button>
+    <button type = "submit" formaction="../Login/login.php">ログイン画面に戻る</button>
 </form>
 </body>
