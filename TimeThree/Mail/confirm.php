86a27512b213b92e9938f51c63bdbca4629f9568
王文星 <117240400+oubunsei@users.noreply.github.com>
2023-06-20 11:55:35 +0900

    Update confirm.php

--- TimeThree/Mail/confirm.php
+++ TimeThree/Mail/confirm.php
@@ -1,9 +1,32 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8" />
-  </head>
+<<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+ <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+ <meta name="viewport" content="width=device-width,initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
+ <meta name="format-detection"content="telephone=no">
+ <meta name="apple-mobile-web-app-capable" content="yes" />
+ <meta name="apple-mobile-web-app-status-bar-style" content="black" />
+<meta http-equir="content-type" charset = "UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" type="text/css" href="../css/acnt_1.css">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
   <body>
+    <style>
+      body{
+       text-align: center;
+       background-color: 	#F0E68C;
+      }
+      button {
+font-family: "MS 明朝"; 
+padding: 5px 10px; 
+display: block;
+margin: auto;
+border-radius: 15px;
+background-color:		#C0C0C0;
+}
+    </style>
     <form>
       <?php
       require '../DAO.php';
@@ -38,9 +61,11 @@
       };
     }else{
       echo 'そのようなメールアドレスは登録されていません。<br><br>
-      入力されたメールアドレス:'.$_POST["mail"].'<br><br>      
+      入力されたメールアドレス:'.$_POST["mail"].'<br><br> 
+      
       <button type = "submit" formaction="mailform.php">メールアドレス入力に戻る</button>
-      <button type = "submit" formaction="../Login/login.php">ログイン画面に戻る</button>';
+      <div class="container mt-2">
+        <button type = "submit" formaction="../Login/login.php">ログイン画面に戻る</button></div>';
     }
 
     echo $content;
