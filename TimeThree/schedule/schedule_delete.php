90514eb584b6e29abb36e7018f128b07faf33409
japanstudent <2101199@s.asojuku.ac.jp>
2023-06-20 12:36:41 +0900

    削除画面の変更6/20
    css追加

--- TimeThree/schedule/schedule_delete.php
+++ TimeThree/schedule/schedule_delete.php
@@ -4,6 +4,88 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+   .example{
+   	width: 100px;
+   }
+  
+   body{
+
+       text-align: center;
+
+       background-color:    #F0E68C;
+
+    }
+
+    h1 {
+
+    font-family: "MS 明朝";      
+
+    text-align:center;
+
+    font-size : 25px;
+
+    line-height : 5;
+
+}
+
+    h2 {
+
+    font-family: "MS 明朝";      
+
+    text-align:center;
+
+    font-size : 25px;
+
+    line-height : 5;
+
+}
+
+button {
+
+font-family: "MS 明朝";      
+
+padding: 10px 15px;
+
+display: block;
+
+margin: auto;
+
+border-radius: 15px;
+
+background-color:       #C0C0C0;
+
+}
+
+input{
+
+  font-family: "MS 明朝";    
+
+  padding: 10px 15px;
+
+  width: 600x;
+
+  border-radius: 10px;
+
+  border: 2px solid black;
+
+  background-color: #C0C0C0;
+
+
+
+
+}
+
+p{
+
+    font-family: "MS 明朝";
+
+
+
+
+}
+
+</style>
 
     <title>Document</title>    
 </head>
