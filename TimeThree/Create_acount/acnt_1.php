d407cdb6c92e489e44c73a06ccb99cd799d60bd0
ウリタ圭佑 <117240590+uritake@users.noreply.github.com>
2023-06-21 10:49:35 +0900

    あ
    ６－２１　最新版

--- TimeThree/Create_acount/acnt_1.php
+++ TimeThree/Create_acount/acnt_1.php
@@ -23,7 +23,7 @@
     h1 {
     font-family: "MS 明朝";  
     text-align:center;
-    font-size : 25px; 
+    font-size : 20px; 
     line-height : 2; 
 }
     h2 {
@@ -47,7 +47,7 @@ input{
   border-radius: 10px;
   border: 2px solid black;
   background-color:	#C0C0C0;
-
+>>>>>>> 5bf2a9697024c7f092581bd035c74963d8f27810
 
 }
 label{
@@ -58,29 +58,25 @@ label{
 <title>新規登録画面</title>
 </head>
 <body class="tema">
-<div class="container mt-4">
+<div class="container mt-1">
 <h1>新規登録画面</h1>
 <div class="mail">
 <form action="?" method="post">
 <label for="mail" class="from-label">メールアドレス</label><br>
 <input type="text" class="from-control" name="mail"placeholder="mailaddress">
 </div>
-<div class="container mt-4">
+<div class="container mt-1">
 <label for="password" class="from-label">パスワード</label><br>
 <input type="password" class="from-control" name="pass"placeholder="password">
 </div>
-<div class="container mt-4">
+<div class="container mt-1">
 <label for="nickname" class="from-label">ニックネーム</label><br>
 <input type="text" class="from-control" name="name"placeholder="ニックネーム">
 </div>
-<div class="container mt-5">
+
 <button type = "submit" formaction="../Login/login.php">ログイン画面へ戻る</button><br>
-</div>
-<div class="container mt-1.5">
 <button type = "submit" formaction="../Create_acount/acnt_2.php">登録する</button><br>
-</div>
 </form>
 </div>
 </body>
 </html>
-
