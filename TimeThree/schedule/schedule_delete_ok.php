c1ee6c1b7b4b2ef39b122bae614e8e4391b20c2f
nakamura2101187 <117244277+nakamura2101187@users.noreply.github.com>
2023-06-28 14:54:38 +0900

    藤井くんのｃｓｓです

--- TimeThree/schedule/schedule_delete_ok.php
+++ TimeThree/schedule/schedule_delete_ok.php
@@ -9,31 +9,11 @@
  <meta name="apple-mobile-web-app-status-bar-style" content="black" />
 <meta http-equir="content-type" charset = "UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="style.css1">
     <title>Document</title>    
 </head>
 <body>
-<style>
-     body{
-       text-align: center;
-       background-color: 	#F0E68C;
-       font-family: "MS 明朝";  
-       font-size : 25px;
-    } 
-    p {
-    font-family: "MS 明朝";  
-    text-align:center;
-    font-size : 30px; 
-    line-height : 2; 
-}
-button {
-font-family: "MS 明朝"; 
-padding: 10px 10px; 
-display: block;
-margin: auto;
-border-radius: 10px;
-background-color:		#C0C0C0;
-}
-</style>
+
 <?php
 session_start();
 
